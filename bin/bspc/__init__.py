525133f84a96231aa376c0c321e31885fe410f77
Lin Xianyi <iynaix@gmail.com>
2020-04-10 18:37:41 +0800

    Default video playing speed is now 1.5x.

--- bin/bspc/__init__.py
+++ bin/bspc/__init__.py
@@ -1,17 +1,21 @@
+import q
 import sys
 
 from .bspc import *
 from .xrandr import XRandR, XRANDR_MONITORS, ENVIRONMENT, ULTRAWIDE_NAME, VERTICAL_NAME
-from .utils import p, rget
+from .utils import rget
 
 
 # capture and print all errors for easier debugging
 def log_exception(exctype, value, tb):
-    p("=======ERROR=======")
-    p("Type:", exctype)
-    p("Value:", value)
-    p("Traceback:", tb)
-    p("===================")
+    if exctype == KeyboardInterrupt:
+        pass
+
+    q("=======ERROR=======")
+    q("Type:", exctype)
+    q("Value:", value)
+    q("Traceback:", tb)
+    q("===================")
 
 
 sys.excepthook = log_exception
